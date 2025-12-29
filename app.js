1c338f4f489b2cc8557ea3290be044b316d862d8
Prakashom18 <shahomprakash097@gmail.com>
2025-12-29 10:03:59 +0545

    Route creation and Mongodb Connection

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ const express = require('express');
 const path = require('path');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const cookieparser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
+const ownerRouter = require('./router/ownerRouter')
+const productRouter = require('./router/productRouter');
+const userRouter = require('./router/userRouter');
+
+const db = require('./config/mongoose-connection');
+
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cookieparser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','ejs');
+
+app.use('/owner',ownerRouter);
+app.use('/users',userRouter);
+app.use('/products',productRouter);
 
 
 app.get('/',(req,res)=>{
--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,10 @@
 const  mongoose = require('mongoose');
 
-mongoose.connect('mongodb//:127.0.0.1:27017/ScatchDb');
-
-const userSchema = mongoose.Schema({
-    fullname : {
-    type : String,
-    minLength : 3,
-    trim : true
-},
-    email : String,
-    password : String,
-    cart : {
-        type : Array,
-        default : []
-    }, 
-    isAdmin : Boolean,
-    orders : {
-        type : Array,
-        default : []
-    },
-    picture : String,
-    contact : Number
+mongoose.connect('mongodb://127.0.0.1:27017/ScatchDb').then
+(function(){
+    console.log("Connected");
+}).catch(function(err){
+    console.log(err)
 })
 
-module.exports = mongoose.module("user",userSchema); 
+module.exports = mongoose.connection
