75d7a82702d7f66e54416f69da28a4c3349c47f1
Prakashom18 <shahomprakash097@gmail.com>
2025-12-31 20:04:05 +0545

    Route creation and Mongodb Connection

--- router/ownerRouter.js
+++ router/ownerRouter.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const ownerModel = require('../models/ownerModel')
+const ownerModel = require('../models/ownerModel');
 
-router.get("/",function(req,res){
+router.get("/", function(req, res) {
     res.send("hey");
-})
+});
+
+if (process.env.NODE_ENV === "development") {
+    router.post('/create', async (req, res) => {
+        // Check request body
+        if (!req.body || Object.keys(req.body).length === 0) {
+            return res.status(400).send("Request body is missing");
+        }
+
+        let owners = await ownerModel.find();
+        if (owners.length > 0) {
+            return res.status(503).send('You don\'t have permission');
+        }
+
+        let { fullname, email, password } = req.body;
+        console.log(req.body);
+
+        // Uncomment to create owner
+        // let createdOwner = await ownerModel.create({ fullname, email, password });
+
+        res.status(201).send("We can create a new owner");
+    });
+}
 
 module.exports = router;
