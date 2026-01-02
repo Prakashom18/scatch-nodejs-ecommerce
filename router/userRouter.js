f5b35881c0ec114a62ae43a680fa6bc36b97b4e7
Prakashom18 <shahomprakash097@gmail.com>
2026-01-02 21:14:40 +0545

    adding cookies

--- router/userRouter.js
+++ router/userRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userModel = require('../models/userModel')
 const router = express.Router();
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 router.get('/',(req,res)=>{
     res.send('user')
@@ -11,22 +12,25 @@ router.post('/register', async (req,res)=>{
     try{
     let { fullname, email, password } = req.body;
     bcrypt.genSalt(10,(err,salt)=>{
-        bcrypt.hash(password,salt,(err,hash)=>{
+        bcrypt.hash(password,salt,async (err,hash)=>{
             if (err) return res.send(err.message)
-            else  { res.send(hash);
+            else  { 
+               let user = await  userModel.create({
+               fullname,
+                email,
+                password :hash
+                
+            })
+            let token = jwt.sign({email,id:user._id},"secretkey")
+            res.cookie("token",token);
+             res.send(token);
             }
         })
     })
         
     
 
-    // let createdUser = await  userModel.create({
-    //    fullname,
-    //     email,
-    //     password
-        
-    // })
-    //  res.send(createdUser)
+    
     }
     catch(err){
         console.log(err.message);
