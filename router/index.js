59000c06305ad2bd0c41aba99a523f8f04090e17
Prakashom18 <shahomprakash097@gmail.com>
2026-01-07 22:24:11 +0545

    Products creation

--- router/index.js
+++ router/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const { isLoggedin } = require('../middlewares/isLoggedin');
 const router = express.Router();
-
+const productModel = require('../models/productModel');
 router.get('/',(req,res)=>{
     let error = req.flash("error");
     res.render('index',{error})
     // res.render('home')
 })
 
-router.get('/shop',isLoggedin,(req,res)=>{
-   let error = req.flash("error")
-    res.render("home",{error});
+router.get('/shop',isLoggedin,async (req,res)=>{
+//    let error = req.flash("error")
+      let product = await productModel.find();
+    res.render("home",{product});
 }
 )
 
